078eef13e9d3f75cd94b35828769dfdc40096bce
Robin Roestenburg <robin@roestenburg.io>
2014-06-12 15:33:45 +0200

    Don't run commit callbacks - they are run correctly after all

--- lib/deferring.rb
+++ lib/deferring.rb
@@ -111,9 +111,6 @@ module Deferring
         # Send the objects of our delegated association to the original
         # association and store the result.
         send(:"original_#{association_name}=", send(:"deferred_#{association_name}").objects)
-        send(:"deferred_#{association_name}").objects.each do |record|
-          record.run_callbacks :commit
-        end
 
         # Store the new value of the association into our delegated association.
         send(
