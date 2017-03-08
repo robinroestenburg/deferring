8dad0fa7f2b2d05e8c6bc4b19436bd5820cf667a
Robin Roestenburg <robin.roestenburg@itrp.com>
2017-03-08 10:15:18 +0100

    Fix `#timestamps` was called without specifying an option for `null` deprecation warning

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 ActiveRecord::Schema.define version: 0 do
   create_table :people do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :people_teams, id: false do |t|
@@ -16,26 +16,26 @@ ActiveRecord::Schema.define version: 0 do
 
   create_table :teams do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :issues do |t|
     t.string :subject
     t.integer :person_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :addresses do |t|
     t.integer :addressable_id
     t.string :addressable_type
     t.string :street
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table :non_validated_issues do |t|
     t.string :subject
     t.integer :person_id
-    t.timestamps
+    t.timestamps null: false
   end
 end
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
